a5948d6a0a2f8f1553db04a8cfb146105edca2e0
Dai Nguyen <dai.thanh.nguyen@gmail.com>
2014-05-29 22:23:32 -0700

    updated

--- DcrOrderEntryTest/Program.cs
+++ DcrOrderEntryTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using DcrDataAccess;
+using DcrDataAccess.Forms;
+using DcrDataAccess.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,9 +11,14 @@ namespace DcrOrderEntryTest
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            
+            SessionInfo info = new SessionInfo("sql_server", "p21_db", "john.doe");
+
+            ErrorForm f = new ErrorForm("Order No Validator", info , "order_no not found");
+            f.ShowDialog();
+
             Console.ReadKey();
         }
     }
